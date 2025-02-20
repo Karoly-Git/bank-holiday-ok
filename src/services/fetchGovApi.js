eb66b2ec2697dff762270d6e3bebb39c45e166c0
Karoly <karoly.webdev@gmail.com>
2025-02-20 08:55:53 +0000

    API fetchData function update

--- src/services/fetchGovApi.js
+++ src/services/fetchGovApi.js
@@ -1,21 +1,21 @@
-/*export default async function fetchData() {
-    try {
-        const response = await fetch("https://www.gov.uk/bank-holidays.json");
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        return { "error": error.message };
-    }
-};*/
-
-
 export default async function fetchData() {
-    await new Promise(resolve => setTimeout(resolve, 2000)); // Delay for 2 seconds
+    try {
+        const response = await fetch("https://www.gov.uk/bank-holidays.json");
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return { "error": error.message };
+    }
+};
+
+
+/*export default async function fetchData() {
+    await new Promise(resolve => setTimeout(resolve, 500)); // Delay
 
     try {
         const response = await fetch("https://www.gov.uk/bank-holidays.json");
@@ -29,6 +29,6 @@ export default async function fetchData() {
     } catch (error) {
         return { "error": error.message };
     }
-}
+}*/
 
 
